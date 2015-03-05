dc6f79ecf0030d0b7406c1baa95a007c8d4dcb64
Marcus Veres <marcus.veres@gmail.com>
2015-03-05 16:13:36 -0500

    adds modal window for get zenbanx :: opens modal with get-zenbanx orange button :: moves zenbanx button to different section

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,5 +1,6 @@
 (function(){
 
+
     "use strict";
 
 
@@ -363,6 +364,58 @@
         }
     ]);
 
+
+    //
+    appControllers.controller(
+        'ModalDemoCtrl', 
+        function( $scope , $modal , $log )
+        {
+            console.log("loading modal controller");
+
+            $scope.items = ['item1', 'item2', 'item3'];
+
+            $scope.open = function (size) {
+
+                var modalInstance = $modal.open({
+                    templateUrl: 'myModalContent.html',
+                    controller: 'ModalInstanceCtrl',
+                    size: size,
+                    resolve: {
+                        items: function () {
+                            return $scope.items;
+                        }
+                    }
+                });
+
+                modalInstance.result.then(function (selectedItem) {
+                    $scope.selected = selectedItem;
+                }, function () {
+                    $log.info('Modal dismissed at: ' + new Date());
+                });
+            };
+            
+        }
+    );
+
+    appControllers.controller(
+        'ModalInstanceCtrl', 
+        function ($scope, $modalInstance, items) 
+        {
+
+            $scope.items = items;
+            $scope.selected = {
+                item: $scope.items[0]
+            };
+
+            $scope.ok = function () {
+                $modalInstance.close($scope.selected.item);
+            };
+
+            $scope.cancel = function () {
+                $modalInstance.dismiss('cancel');
+            };
+        }
+    );
 
 })(angular);
 
