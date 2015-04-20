1446901cf8460b3a2ec07999a7f2c0b95e4994e1
Marcus Veres <marcus.veres@gmail.com>
2015-04-20 14:51:01 -0400

    adding header to test, because IE is a pile of shit

--- header.php
+++ header.php
@@ -16,10 +16,10 @@
     <link rel="icon" href="favicon.ico">
 
     <!-- styles -->
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.1/animate.min.css">
-    <link rel="stylesheet" href="/static/css/styles.min.css"/>
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.1/animate.min.css">
+    <link rel="stylesheet" type="text/css" href="/static/css/styles.min.css"/>
 
     <!-- libs -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
